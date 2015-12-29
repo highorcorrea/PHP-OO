8dbda1d94daa91df8f5be9dbd58fc27191aeb89e
Highor Correa <highorcorrea@hotmail.com>
2015-12-29 21:03:11 -0200

    Criado index.php

---
+++ projeto1/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>POO - Projeto1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+
+<?php
+require_once "Cliente.php"
+
+?>
+
+</body>
+</html>
